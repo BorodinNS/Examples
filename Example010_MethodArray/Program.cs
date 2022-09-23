a381fef4e12ee87ed0396167f60de4791ffd25be
Borodin <Fallenlord48@gmail.com>
2022-09-23 15:31:30 +0300

    Нахождения элемента в массиве, равный заданному

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = {1, 12, 51, 32, 62, 45, 62, 123};
+int n = array.Length;
+int find = 123;
+int index = 0;
+while(index<n)
+{
+    if(array[index] == find)
+        {
+            Console.WriteLine(index);
+            break; // заканчивает алгоритм
+        }
+        index++;
+}
